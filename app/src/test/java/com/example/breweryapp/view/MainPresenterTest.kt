115db6a10333d0d267a53f7be7d4440be338c224
real059mattd <real059mattd@gmail.com>
2022-04-10 17:11:38 -0500

    attempt at unit test yomesayin

--- app/src/test/java/com/example/breweryapp/view/MainPresenterTest.kt
+++ app/src/test/java/com/example/breweryapp/view/MainPresenterTest.kt
@@ -2,7 +2,7 @@ package com.example.breweryapp.view
 
 import android.widget.Button
 import android.widget.SearchView
-import io.mockk.every
+import com.example.breweryapp.service.BreweryService
 import io.mockk.mockk
 import io.mockk.verify
 import junit.framework.Assert.assertEquals
@@ -12,6 +12,7 @@ class MainPresenterTest {
 
     val searchButton: Button = mockk(relaxed = true)
     val view: SearchView = mockk(relaxed = true)
+    val brewService: BreweryService = mockk(relaxed = true)
     val presenter = SearchPresenter(view)
 
     @Test
@@ -19,9 +20,9 @@ class MainPresenterTest {
         assertEquals(3, 2+1)
     }
     @Test
-    fun `On start, display search button`(){
-        presenter.starts
-        verify { searchButton }
+    fun `On start, call getBreweries`(){
+        presenter.startSearch()
+        verify { brewService.getExampleBrewey(any(), any()) }
     }
 
 }
