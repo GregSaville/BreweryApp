77209f83d5dab1da9fa8d2e9f36800249edaffb8
real059mattd <real059mattd@gmail.com>
2022-04-25 13:41:55 -0500

    very nice, how much

--- app/src/main/java/com/example/breweryapp/view/SearchActivity.kt
+++ app/src/main/java/com/example/breweryapp/view/SearchActivity.kt
@@ -2,6 +2,9 @@ package com.example.breweryapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,10 +17,11 @@ class SearchActivity : AppCompatActivity(), SearchView {
     //edit text  && submit button
 
     val presenter = SearchPresenter(this)
-
+    var currentSelection : String = ""
     lateinit var rvBreweries: RecyclerView
     lateinit var spnrSearch: Spinner
-    lateinit var choice: String
+    lateinit var
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
@@ -52,15 +56,45 @@ class SearchActivity : AppCompatActivity(), SearchView {
                 adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spnrSearch.adapter = adapter
+            spnrSearch.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
 
-            spnrSearch.onItemSelectedListener
 
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    val selectedItem = parent?.getItemAtPosition(position).toString()
+                    if (selectedItem == "Name"){
+                        Log.i("asdf", "success")
+                        currentSelection = "Name"
+                    }
+                    if (selectedItem == "City"){
+                        currentSelection = "City"
+                    }
+                    if (selectedItem == "Distance"){
+                        currentSelection = "Distance"
+                    }
+                    if (selectedItem == "Default"){
+                        currentSelection = "Default"
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+            }
+
+
+            }
         }
 
 
     }
 
-}
+
+
 
 
 
